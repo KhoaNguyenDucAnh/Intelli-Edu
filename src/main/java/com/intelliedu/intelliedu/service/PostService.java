9fcc08a600aa5f6ade2b0789ffa3610d9b14d3db
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-07-20 11:51:13 +0700

    Remove Impl structure for Service

--- src/main/java/com/intelliedu/intelliedu/service/PostService.java
+++ src/main/java/com/intelliedu/intelliedu/service/PostService.java
@@ -1,27 +1,91 @@
 package com.intelliedu.intelliedu.service;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.intelliedu.intelliedu.dto.CommentDto;
 import com.intelliedu.intelliedu.dto.PostDto;
+import com.intelliedu.intelliedu.entity.Account;
+import com.intelliedu.intelliedu.entity.Post;
+import com.intelliedu.intelliedu.mapper.PostMapper;
+import com.intelliedu.intelliedu.repository.PostRepo;
+import com.intelliedu.intelliedu.security.service.AuthService;
 
 /**
- * PostService
+ * PostServiceImpl
  */
-public interface PostService {
+@Service
+public class PostService {
 
-  public List<PostDto> findPost(String query);
+  @Autowired
+  private PostRepo postRepo;
 
-  public List<PostDto> findPost(String query, Authentication authentication);
+  @Autowired
+  private PostMapper postMapper;
 
-  public void createPost(PostDto postDto, Authentication authentication);
+  @Autowired
+  private AuthService authService;
 
-  public void updatePost(String title, PostDto postDto, Authentication authentication);
+  public List<PostDto> findPost(String query) {
+    return postMapper.toPostDto(postRepo.findByTitleAndContent(query, query));
+  }
 
-  public void setAnswer(String commentRawId, String postRawId);
+  public List<PostDto> findPost(String query, Authentication authentication) {
+    return postMapper.toPostDto(postRepo.findByTitleAndContentAndAccount(query, query, authService.getAccount(authentication))); 
+  }
 
-  public void deletePost(String title, Authentication authentication);
+  public void createPost(PostDto postDto, Authentication authentication) {
+    Post post = postMapper.toPost(postDto);
+
+    // Set attributes
+    post.setIsAnswered(false);
+    post.setDateTime(OffsetDateTime.now());
+   
+    // Save post to account
+    Account account = authService.getAccount(authentication);
+
+    List<Post> accountPost = account.getPost();
+
+    if (accountPost == null) {
+      accountPost = new ArrayList<>();
+    }
+
+    if (accountPost.stream().anyMatch(tempPost -> tempPost.getTitle().equals(post.getTitle()))) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT);
+    }
+    
+    accountPost.add(post);
+
+    postRepo.save(post);
+  }
+
+  public void updatePost(String title, PostDto postDto, Authentication authentication) {
+    Account account = authService.getAccount(authentication);
+
+    Post post = postRepo
+        .findByTitleAndAccount(title, account)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    if (account.getPost().stream().anyMatch(tempPost -> tempPost.getTitle().equals(postDto.getTitle()))) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT);
+    }
+
+    post = postMapper.toPost(postDto);
+
+    postRepo.save(post);
+  }
+
+  public void setAnswer(String commentRawId, String postRawId) {
+     
+  }
+
+  public void deletePost(String title, Authentication authentication) {
+     
+  }
 }
-
