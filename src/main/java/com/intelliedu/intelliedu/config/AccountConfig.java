f843b87c55948a9b52f67ac99ef317b608f2f43e
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-07-25 15:19:57 +0700

    Remove AccountConfig and MindMapConfig

--- src/main/java/com/intelliedu/intelliedu/config/AccountConfig.java
+++
@@ -1,8 +0,0 @@
-package com.intelliedu.intelliedu.config;
-
-public class AccountConfig {
-  public static final String NOT_FOUND =
-      "Username not found. Please check the username or sign up for a new account.";
-  public static final String CONFLICT =
-      "Account already exists. Please choose a different username or log in if you have an account.";
-}
