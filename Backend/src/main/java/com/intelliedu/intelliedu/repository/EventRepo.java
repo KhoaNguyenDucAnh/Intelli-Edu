85d15b7af4c4549910f39748f85278286dc4f27e
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-08-17 14:51:33 +0700

    Fix incorrect import of Pageable

--- Backend/src/main/java/com/intelliedu/intelliedu/repository/EventRepo.java
+++ Backend/src/main/java/com/intelliedu/intelliedu/repository/EventRepo.java
@@ -2,8 +2,8 @@ package com.intelliedu.intelliedu.repository;
 
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.intelliedu.intelliedu.entity.Account;
