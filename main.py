4175334f30912cdf6daea590986e3faa5d1b95c5
Heinsburg123 <heinsburg2005@gmail.com>
2023-08-22 20:40:24 +0700

    before claude api

--- main.py
+++ main.py
@@ -7,98 +7,185 @@ chainHead=np.zeros((2,100000))
 chainEnd=np.zeros((2,100000))
 chainInd=np.zeros((2,100000))
 values=[[None for i in range(100000)],[None for i in range(100000)]]
-adj=[[[] for i in range(50)],[[] for i in range(50)]]
+adj=[[[] for i in range(100)],[[] for i in range(100)]]
 nBase=np.zeros(2)
 pos=np.zeros((2,100000))
 parent=np.zeros((2,100000))
 nChild=np.zeros((2,100000))
 rev=np.zeros((2,100000))
 flat=[[None for i in range(100000)],[None for i in range(100000)]]
-n=0
+n=1
+d1=0
+d2=0
 model = SentenceTransformer('keepitreal/vietnamese-sbert')
 
 json1={
-  "title": "Conversation",
-  "label":"gay",
-  "nodes": [
-    {
-      "id": "howareyou",
-      "label": "how are u",
-      "nodes": [
-        {
-          "id": "imfine",
-          "label": "i'm fine thank you"
-        },
-        {
-          "id": "andyou",
-          "label": "and you",
-          "nodes": [
-            {
-              "id": "doyouhaveapie",
-              "label": "Do you have a pie"  
-            }
-          ]
-        }
-      ]
-    }
-  ]
+  "title": "Sự thành lập Liên hợp quốc",
+  "root": {
+    "label": "Sự thành lập LHQ",
+    "children": [
+      {
+        "label": "Sự thành lập",
+        "children": [
+          {
+            "label": "Kết thúc WWII"
+          },
+          {
+            "label": "Nguyện vọng hòa bình toàn cầu"  
+          },
+          {
+            "label": "Thỏa thuận tại Hội nghị Yalta"
+          },
+          {
+            "label": "Thông qua Hiến chương tại HNC Phrancisco"  
+          }
+        ]
+      },
+      {
+        "label": "Mục tiêu & Nguyên tắc",
+        "children": [
+          {
+            "label": "Mục tiêu",
+            "children": [
+              { "label": "Duy trì hòa bình" },
+              { "label": "Phát triển quan hệ hữu nghị" },
+              { "label": "Hợp tác quốc tế" }
+            ]
+          },
+          {
+            "label": "Nguyên tắc",
+            "children": [
+              { "label": "Bình đẳng chủ quyền" },
+              { "label": "Toàn vẹn lãnh thổ" }, 
+              { "label": "Không can thiệp nội bộ" },
+              { "label": "Giải quyết tranh chấp hòa bình" },
+              { "label": "Đồng thuận các cường quốc(Liên Xô, Mĩ, Anh, Pháp, Trung Quốc)" }
+            ]
+          }
+        ]
+      },  
+      {
+        "label": "Cơ cấu tổ chức",
+        "children": [
+          { "label": "Đại hội đồng" },
+          { "label": "Hội đồng Bảo an" },
+          { "label": "Hội đồng Kinh tế & Xã hội" }, 
+          { "label": "Hội đồng Quản trị" },
+          { "label": "Tòa án Quốc tế" },
+          { "label": "Ban Thư ký" }
+        ]
+      },
+      {
+        "label": "Trụ sở",
+        "children": [
+          { "label": "Thành phố New York" }
+        ]
+      },
+      {
+        "label": "Vai trò",
+        "children": [
+          { "label": "Diễn đàn hợp tác & đấu tranh cho hòa bình" },
+          { "label": "Giải quyết xung đột" },
+          { "label": "Thúc đẩy quan hệ quốc tế" }
+        ]
+      }
+    ]
+  } 
 }
 
 json2={
-  "title": "Conversation",
-  "label":"gay",
-  "nodes": [
-    {
-      "id": "howareyou",
-      "label": "how are u",
-      "nodes": [
-        {
-          "id": "imfine",
-          "label": "i'm fine thank you"
-        },
-        {
-          "id": "andyou",
-          "label": "and you",
-          "nodes": [
-            {
-              "id": "doyouhaveapie",
-              "label": "Do you have a pie"  
-            }
-          ]
-        }
-      ]
-    }
-  ]
+  "title": "Sự thành lập Liên hợp quốc",
+  "root": {
+    "label": "Sự thành lập LHQ", 
+    "children": [
+      {
+        "label": "Sự thành lập",
+        "children": [
+          {
+            "label": "Kết thúc WW2"  
+          },
+          {
+            "label": "Nguyện vọng hòa bình toàn cầu"
+          }
+        ]
+      },
+      {
+        "label": "Mục tiêu & Nguyên tắc",
+        "children": [
+          {
+            "label": "Mục tiêu", 
+            "children": [
+              { "label": "Duy trì hòa bình thế giới" },
+              { "label": "Phát triển quan hệ hữu nghị" },
+              { "label": "Hợp tác quốc tế" }
+            ]
+          },
+          {
+            "label": "Nguyên tắc",
+            "children": [
+              { "label": "Bình đẳng chủ quyền" }, 
+              { "label": "Toàn vẹn lãnh thổ" },
+              { "label": "Không can thiệp" },
+              { "label": "Giải quyết tranh chấp hòa bình" },
+              { "label": "Đồng thuận các cường quốc (Thái Lan, Mĩ, Anh, Pháp, Trung Quốc" }
+            ]
+          }
+        ]
+      },
+      {
+        "label": "Cơ cấu tổ chức",
+        "children": [
+          { "label": "Đại hội đồng" },
+          { "label": "Hội đồng Bảo an" },
+          { "label": "Hội đồng Kinh tế & Xã hội" },
+          { "label": "Hội đồng Quản trị" },
+          { "label": "Tòa án Quốc tế" },
+          { "label": "Ban Thư ký" }
+        ]
+      },
+      {
+        "label": "Trụ sở",
+        "children": [
+          { "label": "Thành phố New York" }
+        ]
+      },
+      {
+        "label": "Vai trò",
+        "children": [
+          { "label": "Diễn đàn hợp tác & đấu tranh cho hòa bình" }
+        ]  
+      }
+    ]
+  }
 }
-
+json1=json1['root']
+json2=json2['root']
 def to_tree(index,js,u):
     values[index][u]=js['label']
-    global n 
-    n+=1
-    if 'nodes' not in js:
+    if 'children' not in js:
         return
-    js=js['nodes']
-    v=u
+    js=js['children']
+    global n
     for i in range(len(js)):
-        v=v+1
-        adj[index][u].append(v)
-        adj[index][v].append(u)
-        parent[index][v]=u
-        to_tree(index,js[i],v)
-def dfs(u,index):
+        n+=1
+        adj[index][u].append(n)
+        adj[index][n].append(u)
+        parent[index][n]=u
+        to_tree(index,js[i],n)
+def dfs(index,u):
     nChild[index][u]=1
     for v in adj[index][u]:
-        if v!=parent[index][u]:
-            dfs(v,index)
+      if v!=parent[index][u]:
+            dfs(index,v)
             nChild[index][u]+=nChild[index][v]
 
-def hld(u,index):
+def hld(index,u):
     if int(chainHead[index][int(nChain[index])])==0:
         chainHead[index][int(nChain[index])]=u 
     chainInd[index][u]=nChain[index]
     nBase[index]+=1
     pos[index][u]=nBase[index]
-    rev[index][pos[index][u]]=u
+    rev[index][int(pos[index][u])]=u
     spec=-1
     for i in range(len(adj[index][u])):
         v=adj[index][u][i]
@@ -106,49 +193,58 @@ def hld(u,index):
             if(spec==-1 or nChild[index][v]>nChild[index][spec]):
                 spec=v
     if(spec>-1):
-        hld(spec,index)
+        hld(index,spec)
     if(spec==-1):
         chainEnd[index][int(nChain[index])]=u
     for v in adj[index][u]:
         if(v!=parent[index][u] and v!=spec):
             nChain[index]+=1
-            hld(v,index)
+            hld(index,v)
 
-def flatten(index):
+def flatten(index,n):
   for i in range(n):
     flat[index][int(pos[index][i])]=values[index][i]
 
-def find(text):
-  corpus_em=model.encode(flat[1])
+def find(text,n):
+  corpus_em=model.encode(flat[1][1:n+1])
+  text=model.encode(text)
   hits = util.semantic_search(text, corpus_em, score_function=util.dot_score)
   position=hits[0][0]['corpus_id']
   return position
 
-to_tree(0,json,0)
-to_tree(1,json,0)
-dfs(0,0)
-dfs(1,0)
-hld(0,0)
-hld(1,0)
-flatten(0)
-flatten(1)
-
-for i in range(nChain[0]):
-  findHead=find(chainHead[0][i])
-  findEnd=find(chainEnd[0][i])
-  comp_text=flat[0][chainHead[0][i]:chainEnd[0][i]]
-  Head_index=rev[0][findHead]
-  End_index=rev[0][findEnd]
-  user_text=''
+to_tree(0,json1,1)
+d1=n
+n=1
+to_tree(1,json2,1)
+d2=n
+dfs(0,1)
+dfs(1,1)
+hld(0,1)
+hld(1,1)
+flatten(0,d1)
+flatten(1,d2)
+for i in range(int(nChain[0])):
+  findHead=int(find(values[0][int(chainHead[0][i])],d2))+1
+  findEnd=int(find(values[0][int(chainEnd[0][i])],d2))+1
+  comp_text=flat[0][int(pos[0][int(chainHead[0][i])]):(int(pos[0][int(chainEnd[0][i])])+1)]
+  Head_index=int(rev[1][findHead])
+  End_index=int(rev[1][findEnd])
+  user_text=[]
+  if(End_index==Head_index):
+    user_text=flat[1][int(pos[1][int(Head_index)]):int(pos[1][int(End_index)])+1]
+    print(comp_text,user_text)
+  loca=0
   while(End_index!=Head_index):
-    user_chain=chainInd[0][End_index]
-    user_text+=flat[0][pos[0][chainHead[0][user_chain]]:pos[0][End_index]]
-    End_index=parent[0][chainHead[0][user_chain]]   
-    user_chain=chainInd[0][End_index]
-
-
-        
-
-
-
-
+    user_chain=int(chainInd[1][int(End_index)])
+    if(chainInd[1][int(End_index)]==chainInd[1][int(Head_index)]):
+      user_text=flat[1][int(pos[1][int(Head_index)]):int(pos[1][int(End_index)])+1]+user_text
+      break
+    user_text=flat[1][int(pos[1][int(chainHead[1][int(user_chain)])]):int(pos[1][int(End_index)])+1]+user_text
+    End_index=int(parent[1][int(chainHead[1][user_chain])]) 
+    if(End_index==0):
+      loca=-1
+      break
+    user_chain=int(chainInd[1][int(End_index)])
+  if(loca==-1):
+    print('sai vi tri dang le',flat[1][findHead],'va',flat[1][findEnd],'o cung 1 chuoi')
+  print(comp_text,user_text)
