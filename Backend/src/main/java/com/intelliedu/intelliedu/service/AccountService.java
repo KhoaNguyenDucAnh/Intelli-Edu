7634f156b25974ba2267ceaa41fee2885da6a8e8
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-09-11 12:33:56 +0700

    Add point

--- Backend/src/main/java/com/intelliedu/intelliedu/service/AccountService.java
+++ Backend/src/main/java/com/intelliedu/intelliedu/service/AccountService.java
@@ -1,10 +1,13 @@
 package com.intelliedu.intelliedu.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.intelliedu.intelliedu.dto.AccountResponseDto;
+import com.intelliedu.intelliedu.entity.Account;
 import com.intelliedu.intelliedu.mapper.AccountMapper;
 import com.intelliedu.intelliedu.repository.AccountRepo;
 import com.intelliedu.intelliedu.security.service.AuthService;
@@ -24,4 +27,29 @@ public class AccountService {
   public AccountResponseDto getAccountInfo(Authentication authentication) {
     return accountMapper.toAccountResponseDto(authService.getAccount(authentication));
   }
+
+	public Integer increasePoint(Integer point, Authentication authentication) {
+		Account account = authService.getAccount(authentication);
+
+		if (point < 0) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+
+		account.setPoint(account.getPoint() + point);
+
+		return accountRepo.save(account).getPoint();
+	}
+
+	public Integer decreasePoint(Integer point, Authentication authentication) {
+		Account account = authService.getAccount(authentication);
+
+		if (point < 0 || account.getPoint() < point) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+
+		account.setPoint(account.getPoint() - point);
+
+		return accountRepo.save(account).getPoint();
+
+	}
 }
