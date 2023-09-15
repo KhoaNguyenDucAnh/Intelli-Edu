f58f9b1a6e146c347cf0f007c99470bb2d7dc9d9
Heinsburg123 <heinsburg2005@gmail.com>
2023-09-15 12:21:50 +0700

    add compare func

--- AI/main.py
+++ AI/main.py
@@ -17,7 +17,13 @@ flat=[[None for i in range(100000)],[None for i in range(100000)]]
 n=1
 d1=0
 d2=0
+
 model = SentenceTransformer('keepitreal/vietnamese-sbert')
+
+from claude_api import Client
+import os
+cookie='intercom-device-id-lupk8zyo=435acf1c-a907-4409-ae7b-593d8b45e4a5; __cf_bm=0meTOJSogp4qPQ_1VBmhqL5Lamyy_.g4mIHT4BQQn3s-1694107625-0-AQ/1ogiV1p4p1e2Y1Hkt3NNCmfIYFK8w265IOjNuWWsKbw88zFa+DKEWyHN2iLNDJFvFfnOzFlkGNc5BxNls0ho=; cf_clearance=yPRpEXnThZTOnPyDzIgSRdPC3xrw3KtgIED3ftmDoMU-1694107627-0-1-2d56ae04.7f0bbe63.7a57f1e4-0.2.1694107627; sessionKey=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; intercom-session-lupk8zyo=dEVWZk9WMmlFdWJ0cUlhZXZ4S2szcnhYL29wRjB3Nis1RXdBcFVLUWdCSEZHRThCdmlETEZHcG0yVjZVUjRlaS0tR3FCVHc4UlpwNE5VRGFKeXV0bDZiZz09--f5244f8d1d57e8176b67dc3c50030a39fee7dcf3'
+claude_api=Client(cookie)
 
 json1={
   "title": "Sự thành lập Liên hợp quốc",
@@ -217,6 +223,15 @@ def find(text,n):
   hits = util.semantic_search(text, corpus_em, score_function=util.dot_score)
   position=hits[0][0]['corpus_id']
   return position
+def compare(ct,up):
+  prompt=''
+  new_chat = claude_api.create_new_chat()
+  conversation_id = new_chat['uuid']
+  prompt+=ct+' '+up+'\n'
+  prompt+='Can you see if the second text is missing any information or numbers or wrong order compare to the first text'
+  conversation_id = conversation_id
+  response = claude_api.send_message(prompt, conversation_id)
+  return response
 
 to_tree(0,json1,1)
 d1=n
@@ -238,7 +253,7 @@ for i in range(int(nChain[0])):
   user_text=[]
   if(End_index==Head_index):
     user_text=flat[1][int(pos[1][int(Head_index)]):int(pos[1][int(End_index)])+1]
-    print(comp_text,user_text)
+    print(compare(comp_text,user_text))
   loca=0
   while(End_index!=Head_index):
     user_chain=int(chainInd[1][int(End_index)])
@@ -253,4 +268,4 @@ for i in range(int(nChain[0])):
     user_chain=int(chainInd[1][int(End_index)])
   if(loca==-1):
     print('sai vi tri dang le',flat[1][findHead],'va',flat[1][findEnd],'o cung 1 chuoi')
-  print(comp_text,user_text)
+  print(compare(comp_text,user_text))
