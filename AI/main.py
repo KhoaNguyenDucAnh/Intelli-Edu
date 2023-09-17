267080f9d7f722a8545cd2ffb3c5a654b6d6eefa
Heinsburg123 <heinsburg2005@gmail.com>
2023-09-17 17:23:06 +0700

    done add compare function

--- AI/main.py
+++ AI/main.py
@@ -22,7 +22,7 @@ model = SentenceTransformer('keepitreal/vietnamese-sbert')
 
 from claude_api import Client
 import os
-cookie='intercom-device-id-lupk8zyo=435acf1c-a907-4409-ae7b-593d8b45e4a5; __cf_bm=0meTOJSogp4qPQ_1VBmhqL5Lamyy_.g4mIHT4BQQn3s-1694107625-0-AQ/1ogiV1p4p1e2Y1Hkt3NNCmfIYFK8w265IOjNuWWsKbw88zFa+DKEWyHN2iLNDJFvFfnOzFlkGNc5BxNls0ho=; cf_clearance=yPRpEXnThZTOnPyDzIgSRdPC3xrw3KtgIED3ftmDoMU-1694107627-0-1-2d56ae04.7f0bbe63.7a57f1e4-0.2.1694107627; sessionKey=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; intercom-session-lupk8zyo=dEVWZk9WMmlFdWJ0cUlhZXZ4S2szcnhYL29wRjB3Nis1RXdBcFVLUWdCSEZHRThCdmlETEZHcG0yVjZVUjRlaS0tR3FCVHc4UlpwNE5VRGFKeXV0bDZiZz09--f5244f8d1d57e8176b67dc3c50030a39fee7dcf3'
+cookie='intercom-device-id-lupk8zyo=435acf1c-a907-4409-ae7b-593d8b45e4a5; __cf_bm=v6JqButMzKBvfpTQXoJTrFdg8h.CA.L4ELkCO4K665w-1694945465-0-ATdUa52goRTNwsvZayww9rer2sGOMP3E1gwX8nZD0mQkgrmADeiIFTPZUfTntdHsA1tLtBNuh6R4k5KbPnMHY04=; cf_clearance=yP6Y7jTzeRGd2O_HnKCEKFToerLmWY8GLP0FojwkK0Q-1694945468-0-1-2d56ae04.e69e4851.7a57f1e4-0.2.1694945468; sessionKey=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; intercom-session-lupk8zyo=VTRoNlBWOUpoNDdEYXAvWGJNOHdGUThVNGhEUlFicFBEWWRxanMyWlhmbFh1Q0xoVThQQldwVDhoYjhqbVhNOC0tZGVMV1lPQ3Y2bmZzbGo0VU1zL2hOQT09--00f859741b60b442d2c9c692d09479ec9e329c09'
 claude_api=Client(cookie)
 
 json1={
@@ -224,11 +224,10 @@ def find(text,n):
   position=hits[0][0]['corpus_id']
   return position
 def compare(ct,up):
-  prompt=''
   new_chat = claude_api.create_new_chat()
   conversation_id = new_chat['uuid']
-  prompt+=ct+' '+up+'\n'
-  prompt+='Can you see if the second text is missing any information or numbers or wrong order compare to the first text'
+  prompt=str(ct)+' '+str(up)+'\n'
+  prompt+='Can you see if the second text is missing any information or numbers or wrong order compare to the first text in less than 5 sentences'
   conversation_id = conversation_id
   response = claude_api.send_message(prompt, conversation_id)
   return response
