572ad0322bd392e21363c1d11e83a6be6f0046ce
tDep <tiep2403nbk@gmail.com>
2023-11-13 22:08:41 +0700

    update homepage

--- Frontend/homepage/src/Components/Comeherebutton.js
+++ Frontend/homepage/src/Components/Comeherebutton.js
@@ -2,7 +2,10 @@
 function Comeherebutton() {
     return(
         <div className="comehereButton">
-            <p className="comehereText">Đến ngay →</p>
+            <div className="comehereText">
+                Đến ngay
+                <img className="comehereTextArrow" src={require("../images/rightarrow.png")} alt="bruh"/>
+            </div>
             <div className='comehereBackground'/>
         </div>
     )
--- Frontend/homepage/src/Components/Header.js
+++ Frontend/homepage/src/Components/Header.js
@@ -9,9 +9,6 @@ function Header() {
                     <div className="navigation">
                     <div className="navItem">
                             <p className="navText">Tài liệu</p>
-                            <span class="material-symbols-outlined downArrow">
-                                expand_more
-                            </span>
                         </div> 
                         <div className="withArrow navItem">
                             <p className="navText">Công cụ hỗ trợ</p>
@@ -20,16 +17,13 @@ function Header() {
                             </span>
                             <div className="toolListWrapper">
                                 <div className="toolList">
-                                    <p className="toolListItem">Chatbot</p>
+                                    <p className="toolListItem">Mô hình ngôn ngữ lớn</p>
                                     <p className="toolListItem">Sơ đồ tư duy</p>
                                 </div>
                             </div>
                         </div> 
                         <div className="noArrow navItem">
                             <p className="navText">Kế hoạch</p>
-                        </div>
-                        <div className="noArrow navItem">
-                            <p className="navText">Giải trí</p>
                         </div>
                         <div className="noArrow navItem">
                             <p className="navText">Bảng xếp hạng</p>
