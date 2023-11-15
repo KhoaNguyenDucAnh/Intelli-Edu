86286c42a5a01a52c4e07f00e6d7e315a0d06cc8
tuanliz0403 <tuanliz0403@gmail.com>
2023-11-15 21:26:56 +0700

    'addedPagination'

--- Frontend/classbook/src/index.js
+++ Frontend/classbook/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './views/App';
+import { Select } from '@mantine/core';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
