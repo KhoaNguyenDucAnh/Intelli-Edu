146c47acc531d26dabae1b4fdb567f1eaa269127
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-11-25 03:31:21 +0700

    Disallow CORS

--- Backend/src/main/java/com/intelliedu/intelliedu/security/WebSecurityConfig.java
+++ Backend/src/main/java/com/intelliedu/intelliedu/security/WebSecurityConfig.java
@@ -1,11 +1,8 @@
 package com.intelliedu.intelliedu.security;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,9 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.intelliedu.intelliedu.config.SecurityConfig;
 import com.intelliedu.intelliedu.security.exception.CustomAuthenticationEntryPoint;
@@ -38,21 +32,8 @@ public class WebSecurityConfig {
   private JWTFilter jwtFilter;
 
   @Bean
-  @Profile("test")
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("*"));
-    configuration.setAllowedHeaders(Arrays.asList("*"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-
-  @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
       .csrf(CsrfConfigurer::disable)
       .authenticationProvider(authenticationProvider)
       .exceptionHandling(exception -> exception.authenticationEntryPoint(customAuthenticationEntryPoint))
