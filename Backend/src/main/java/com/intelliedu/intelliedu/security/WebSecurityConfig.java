af2c6b4f1dc1d9474d26abf878b31f6c533fbd9c
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-12-04 11:12:43 +0700

    Anonymous AuthenticationEntryPoint

--- Backend/src/main/java/com/intelliedu/intelliedu/security/WebSecurityConfig.java
+++ Backend/src/main/java/com/intelliedu/intelliedu/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.intelliedu.intelliedu.security;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,25 +10,27 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.intelliedu.intelliedu.config.SecurityConfig;
-import com.intelliedu.intelliedu.security.exception.CustomAuthenticationEntryPoint;
 import com.intelliedu.intelliedu.security.filter.JWTFilter;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
   @Autowired
   private DaoAuthenticationProvider authenticationProvider;
-
-  @Autowired
-  private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
   
   @Autowired
   private JWTFilter jwtFilter;
@@ -36,7 +40,13 @@ public class WebSecurityConfig {
     http
       .csrf(CsrfConfigurer::disable)
       .authenticationProvider(authenticationProvider)
-      .exceptionHandling(exception -> exception.authenticationEntryPoint(customAuthenticationEntryPoint))
+      .exceptionHandling(exception -> exception.authenticationEntryPoint(new AuthenticationEntryPoint() {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+          log.error(String.format("Unauthorized access from %s to %s", request.getRemoteHost(), request.getRequestURL()));
+          response.sendRedirect("/login");
+        }
+      }))
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth -> auth
         .requestMatchers(new RequestMatcher() {
