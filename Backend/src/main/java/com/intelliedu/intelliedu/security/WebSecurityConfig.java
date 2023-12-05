40a6671140fbff9f4c7df663c2cc3984382c5bd0
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-12-05 17:23:51 +0700

    Update Security
    Rearrange components

--- Backend/src/main/java/com/intelliedu/intelliedu/security/WebSecurityConfig.java
+++ Backend/src/main/java/com/intelliedu/intelliedu/security/WebSecurityConfig.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -18,6 +22,7 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.intelliedu.intelliedu.config.SecurityConfig;
 import com.intelliedu.intelliedu.security.filter.JWTFilter;
+import com.intelliedu.intelliedu.security.service.UserDetailsServiceImpl;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -28,37 +33,64 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-  @Autowired
-  private DaoAuthenticationProvider authenticationProvider;
   
   @Autowired
   private JWTFilter jwtFilter;
+
+  @Autowired
+  private UserDetailsServiceImpl userDetailsServiceImpl;
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
+  }
+
+  @Bean
+  DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsServiceImpl);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  AuthenticationEntryPoint authenticationEntryPoint() {
+    return new AuthenticationEntryPoint() {
+      @Override
+      public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.error(String.format("Unauthorized access from %s to %s", request.getRemoteHost(), request.getRequestURL()));
+        response.sendRedirect("/login");
+      }
+    };
+  }
+
+  @Bean
+  RequestMatcher requestMatcher() {
+    return new RequestMatcher() {
+      @Override
+      public boolean matches(HttpServletRequest request) {
+        return request.getRequestURI().matches(SecurityConfig.PERMITALL_REGEX)
+            || (request.getMethod().equals("GET") && request.getRequestURI().matches(SecurityConfig.PERMITALLWITHAUTH_REGEX));
+      }
+    };
+  }
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
       .csrf(CsrfConfigurer::disable)
-      .authenticationProvider(authenticationProvider)
-      .exceptionHandling(exception -> exception.authenticationEntryPoint(new AuthenticationEntryPoint() {
-        @Override
-        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-          log.error(String.format("Unauthorized access from %s to %s", request.getRemoteHost(), request.getRequestURL()));
-          response.sendRedirect("/login");
-        }
-      }))
+      .authenticationProvider(authenticationProvider())
+      .exceptionHandling(exception -> exception.authenticationEntryPoint(authenticationEntryPoint()))
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth -> auth
-        .requestMatchers(new RequestMatcher() {
-          @Override
-          public boolean matches(HttpServletRequest request) {
-            return request.getRequestURI().matches(SecurityConfig.PERMITALL_REGEX)
-                || (request.getMethod().equals("GET") && request.getRequestURI().matches(SecurityConfig.PERMITALLWITHAUTH_REGEX)); 
-          }
-        }).permitAll()
+        .requestMatchers(requestMatcher()).permitAll()
         .anyRequest().authenticated())
       .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
     return http.build();
   }
 }
